1b7f2a4c6144afb3c4b99176229297b0e4d0da7c
srirampally-newellco <sri.rampally@newellco.com>
2025-10-30 17:23:26 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
-Our mini FastAPI app place holder
+# Our mini FastAPI app place holder
+from fastapi import Fatsapi
+app = Fastapi()
+
+@app.get("/")
+def read_root():
+  return {"message": " Test app deployed successfully}
