5771643411ff130c3dee0218512c1d5a71f3eb94
srirampally-newellco <sri.rampally@newellco.com>
2025-10-30 22:37:40 -0400

    Update and rename main.py to app.py

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 # Our mini FastAPI app place holder
 from fastapi import FastAPI
-import uvicorn
 
 app = FastAPI()
 
 @app.get("/")
-def home():
+def root():
   return {"message": " Test app deployed successfully"}
-if __name__ == "__main__":
-  uvicorn.run(app, host= "0.0.0.0", port=8000)
+
