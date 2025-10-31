b40737305546f7633342baa0548e27075334f5a4
srirampally-newellco <sri.rampally@newellco.com>
2025-10-30 22:18:06 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 # Our mini FastAPI app place holder
-from fastapi import Fatsapi
-app = Fastapi()
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
 
 @app.get("/")
-def read_root():
+def home():
   return {"message": " Test app deployed successfully"}
+if __name__ == "__main__":
+  uvicorn.run(app, host= "0.0.0.0", port=8000)
